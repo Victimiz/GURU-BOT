d9bc742aafb11a0365326e73e9002c3a065240dc
Victimiz <123818157+Victimiz@users.noreply.github.com>
2023-12-11 23:00:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['919098723453', 'Victimized Xd', true],
+  ['919516643403', 'Ash', true], 
   [''] 
 ] //Number of owners
 
@@ -47,8 +47,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'Victimized Xd' 
+global.author = 'Victimized Xd' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
